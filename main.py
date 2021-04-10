dd2a866ed2892af2f3638d0f7bd36e10ba160739
lit9999999999 <>
2021-04-10 04:56:56 +0000

    Finalized

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import turtle
-screen = turtle.Screen()
+
+wn = turtle.Screen()
 turtle = turtle.Turtle()
+
 turtle.hideturtle()
-screen.bgcolor('black')
-screen.screensize(1000,150)
-turtle.color('yellow')
+wn.bgcolor('black')
+wn.setup(width=1500, height=500)
+turtle.color('sky blue')
 player1 = int(8000)
 player2 = int(8000)
 player1name = input("What is player 1's name?")
 player2name = input("What is player 2's name?")
-turtle.goto(-100, 0)
-turtle.write("{}: {}".format(player1name,player1), align="center", font=("Courier", 20, "normal"))
-turtle.goto(100,0)
-turtle.write("{} : {}".format(player2name, player2), align="center", font=("Courier", 20, "normal"))
+turtle.goto(-200, 0)
+turtle.write("{}: {}".format(player1name,player1), align="center", font=("Courier", 40, "normal"))
+turtle.goto(200,0)
+turtle.write("{} : {}".format(player2name, player2), align="center", font=("Courier", 40, "normal"))
 
 while True:
   add = input("Type 1 or 2 depending on which player is gaining a point : ")
@@ -22,27 +24,27 @@ while True:
   if add == '1':
     turtle.clear()
     player1+=SUBTRACT
-    turtle.goto(-100, 0)
-    turtle.write("{}: {}".format(player1name,player1), align="center", font=("Courier", 20, "normal"))
-    turtle.goto(100,.0)
-    turtle.write("{} : {}".format(player2name, player2), align="center", font=("Courier", 20, "normal"))
+    turtle.goto(-200, 0)
+    turtle.write("{}: {}".format(player1name,player1), align="center", font=("Courier", 40, "normal"))
+    turtle.goto(200,0)
+    turtle.write("{} : {}".format(player2name, player2), align="center", font=("Courier", 40, "normal"))
   elif add == '2':
     player2+=SUBTRACT
     turtle.clear()
     player2+=1
-    turtle.goto(-100, 0)
-    turtle.write("{}: {}".format(player1name,player1), align="center", font=("Courier", 20, "normal"))
-    turtle.goto(100,.0)
-    turtle.write("{} : {}".format(player2name, player2), align="center", font=("Courier", 20, "normal"))
+    turtle.goto(-200, 0)
+    turtle.write("{}: {}".format(player1name,player1), align="center", font=("Courier", 40, "normal"))
+    turtle.goto(200,.0)
+    turtle.write("{} : {}".format(player2name, player2), align="center", font=("Courier", 40, "normal"))
   else:
     print("Invalid")
 
   if player1 < 1:
     turtle.clear()
     turtle.goto(0,0)
-    turtle.write("{} wins!".format(player2name), align="center", font=("Courier", 20, "normal"))
+    turtle.write("✨{} wins!✨".format(player2name), align="center", font=("Courier", 100, "normal"))
     
   elif player2 < 1:
     turtle.clear()
     turtle.goto(0,0)
-    turtle.write("{} wins!".format(player1name), align="center", font=("Courier", 20, "normal"))
+    turtle.write("✨{} wins!✨".format(player1name), align="center", font=("Courier", 100, "normal"))
